d7e66c79b3814aeeed9904649b3a972c93175474
Amine Neggazi <neggazimedlamine@gmail.com>
2023-01-13 13:11:12 +0100

    fixed footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="px-4 sm:text-l md:text-xl bg-primary text-secondary text-center p-4 font-bold">Footer</footer>
+    <footer className="px-4 sm:text-l md:text-xl bg-primary text-secondary text-center p-4 font-bold">
+      Footer
+    </footer>
   )
 }
 

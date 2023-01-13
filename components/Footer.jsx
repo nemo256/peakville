02eb4f196a5288b79f3a4693de8e381370dfa704
Amine Neggazi <neggazimedlamine@gmail.com>
2023-01-13 16:54:50 +0100

    almost finished the login page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="px-4 sm:text-l md:text-l bg-primary text-[#B0B0B0] text-center p-4 font-bold">
+    <footer className="px-4 sm:text-l md:text-l bg-primary text-[#909090] text-center p-4 font-bold">
       ©2023 Game Company. All rights reserved.
     </footer>
   )
--- pages/login.jsx
+++ pages/login.jsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import Router from 'next/router'
 import { useState, useEffect } from "react";
 
+// custom imports
+import Footer from '../components/Footer.jsx'
+
 const Login = () => {
   const [error, setError] = useState('')
   const [isAuthenticated, setIsAuthenticated] = useState(null)
@@ -20,10 +23,65 @@ const Login = () => {
         <meta name="description" content="Retro-terminal web app" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className="max-w-screen-xl text-white mx-auto flex flex-col h-screen justify-between scroll-smooth">
-        <section className="sm:text-l md:text-xl bg-primary text-accent text-center p-4 font-bold">
-          Login
-        </section>
+      <div className="max-w-screen-xl text-white mx-auto flex flex-col h-screen justify-between">
+        <div className="flex-grow sm:text-sm md:text-sm text-secondary p-4 text-center font-bold">
+          <div className="pt-16 flex justify-center items-center h-full">
+            <div className="block p-6 rounded-lg shadow-lg max-w-sm">
+              <img className="pb-10" src="/peakville.png" alt="Unknown image" />
+              <form>
+                <div className="form-group mb-4">
+                  <input type="username" className="form-control
+                    block
+                    w-full
+                    px-4
+                    py-2
+                    placeholder-gray-700
+                    text-base
+                    text-green-900
+                    bg-[#0B0C0B] bg-clip-padding
+                    border-2 border-solid border-gray-800
+                    transition
+                    ease-in-out
+                    m-0
+                    hover:text-green-900 hover:bg-[#090909] hover:border-green-900 outline-none
+                    focus:text-green-900 focus:bg-[#090909] focus:border-green-900 outline-none"
+                    aria-describedby="usernameHelp" placeholder="Username" />
+                </div>
+                <div className="form-group mb-6">
+                  <input type="password" className="form-control
+                    block
+                    w-full
+                    px-4
+                    py-2
+                    placeholder-gray-700
+                    text-base
+                    text-green-900
+                    bg-[#0B0C0B] bg-clip-padding
+                    border-2 border-solid border-gray-800
+                    transition
+                    ease-in-out
+                    m-0
+                    hover:text-green-900 hover:bg-[#090909] hover:border-green-900 outline-none
+                    focus:text-green-900 focus:bg-[#090909] focus:border-green-900 outline-none"
+                    aria-describedby="passwordHelp" placeholder="Password" />
+                </div>
+                <button type="submit" className="
+                  w-full
+                  py-2.5
+                  bg-green-900
+                  text-[#B0B0B0]
+                  leading-tight
+                  hover:bg-green-800 hover:shadow-lg hover:text-[#E0E0E0]
+                  focus:bg-green-800 focus:shadow-lg focus:outline-none focus:ring-0
+                  active:bg-blue-800 active:shadow-lg
+                  transition
+                  duration-150
+                  ease-in-out">Log In</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <Footer />
       </div>
     </>
   )
